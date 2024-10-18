fce2ea9176513478eb84147fb5f9f381149492c2
tsteele1 <tylersteele557@gmail.com>
2024-10-18 08:44:48 -0600

    Attempting to merge with Malhar.

--- Assets/Scripts/Combat/Board.cs
+++ Assets/Scripts/Combat/Board.cs
@@ -2,18 +2,109 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// FIXME: Change the state transitions such that:
-// 1. Animations actually work
-// 2. All effects apply before player attacks
-
 public class Board : MonoBehaviour
 {
     private static Board _instance;
     public static Board Instance { get { return _instance; } }
     public List<List<BoardPosition>> board = new List<List<BoardPosition>>();
     public GameObject uiCanvas;
+    private List<CardInfo> enemyCards;
+    private List<CardInfo> keyCards;
     public List<CardInfo> startingCards;
-    public int fightIndex = 0;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Start()
+    {
+        // Start a coroutine to wait for enemy cards to be ready
+        StartCoroutine(InitializeBoardWhenCardsReady());
+    }
+
+    private IEnumerator InitializeBoardWhenCardsReady()
+    {
+        // Wait until CombatSceneManager has the enemy and key cards ready
+        while (CombatSceneManager.Instance == null ||
+               CombatSceneManager.Instance.GetEnemyCards() == null ||
+               CombatSceneManager.Instance.GetEnemyCards().Count == 0 ||
+               CombatSceneManager.Instance.GetKeyCards() == null ||
+               CombatSceneManager.Instance.GetKeyCards().Count == 0)
+        {
+            yield return null; // Wait for the next frame
+        }
+
+        // Get the enemy cards and key cards from CombatSceneManager
+        enemyCards = CombatSceneManager.Instance.GetEnemyCards();
+        keyCards = CombatSceneManager.Instance.GetKeyCards();
+
+        // Now proceed with the board initialization
+        for (int i = 0; i < 3; i++)
+        {
+            board.Add(new List<BoardPosition>());
+
+            for (int j = 1; j < 4; j++)
+            {
+                BoardPosition pos = uiCanvas.transform.GetChild(i * 3 + j).gameObject.GetComponent<BoardPosition>();
+                pos.setBoardPosition(i, j - 1);
+                board[i].Add(pos);
+
+                if (i == 0)
+                {
+                    // Assign enemy cards using the enemyCards list
+                    assignEnemy(i, j, pos.transform);
+                }
+                else if (i == 2)
+                {
+                    // Assign hero cards using the orderedKeyCards list
+                    assignHero(i, j, pos.transform);
+                }
+            }
+        }
+    }
+
+
+    public void removeCard(int row, int col)
+    {
+        board[row][col].clearCard();
+    }
+
+    // Modified to use the enemy cards from CombatSceneManager instead of startingCards
+    public void assignEnemy(int row, int col, Transform parentTransform)
+    {
+        if (enemyCards != null && enemyCards.Count >= col) // Ensure enough cards are available
+        {
+            GameObject card = HandHandler.Instance.generateCard(enemyCards[col - 1], parentTransform);
+            card.GetComponent<CardMover>().locked = true; // prevent this card from being dragged
+            board[row][col - 1].setCard(card.GetComponent<CardView>());
+        }
+        else
+        {
+            Debug.LogError("Not enough enemy cards available in CombatSceneManager.");
+        }
+    }
+
+    public void assignHero(int row, int col, Transform parentTransform)
+    {
+        if (keyCards != null && keyCards.Count >= col)
+        {
+            GameObject card = HandHandler.Instance.generateCard(keyCards[col - 1], parentTransform);
+            card.GetComponent<CardMover>().locked = true; // prevent this card from being dragged
+            board[row][col - 1].setCard(card.GetComponent<CardView>());
+
+        }
+        else
+        {
+            Debug.LogError("Not enough hand cards available in CombatSceneManager.");
+        }
+
+    }
+
+
+
+
+public int fightIndex = 0;
 
     // Tiny finite state machine for combat
     public enum combatPhase
@@ -31,43 +122,10 @@ public class Board : MonoBehaviour
     public combatPhase currentPhase;
     public combatPhase previousPhase;
 
-    void Awake()
-    {
-        _instance = this;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            board.Add(new List<BoardPosition>());
-            
-            for (int j = 1; j < 4; j++)
-            {
-                BoardPosition pos = uiCanvas.transform.GetChild(i * 3 + j).gameObject.GetComponent<BoardPosition>();
-                pos.setBoardPosition(i, j - 1);
-                board[i].Add(pos);
-
-                if (i == 0)
-                {
-                    assignEnemy(i, j, pos.transform);
-                }
-                else if (i == 2)
-                {
-                    assignHero(i, j, pos.transform);
-                }
-            }
-        }
-
-        currentPhase = combatPhase.Play;
-        previousPhase = combatPhase.Play;
-    }
-
     // Update is called once per frame
     void Update()
     {
-        switch(currentPhase)
+        switch (currentPhase)
         {
             case combatPhase.Play:
                 // Currently: Nothing we need to do.
@@ -103,31 +161,8 @@ public class Board : MonoBehaviour
         }
     }
 
-    public void removeCard(int row, int col)
-    {
-        board[row][col].clearCard();
-    }
-
-    // FIXME: assign the enemy saved from Map here instead of one from the starting cards list
-    // (or just make the starting cards list the cards you saved)
-    public void assignEnemy(int row, int col, Transform parentTransform)
-    {
-        GameObject card = HandHandler.Instance.generateCard(startingCards[col-1], parentTransform);
-        card.GetComponent<CardMover>().locked = true; // prevent this card from being dragged
-        board[row][col-1].setCard(card.GetComponent<CardView>());
-    }
-
-    // FIXME: assign the hero (enzo, claudio, sofia, etc.) saved from Map here instead of one from the starting cards list
-    // (or just make the starting cards list the cards you saved)
-    public void assignHero(int row, int col, Transform parentTransform)
-    {
-        GameObject card = HandHandler.Instance.generateCard(startingCards[2 + col], parentTransform);
-        card.GetComponent<CardMover>().locked = true; // prevent this card from being dragged
-        board[row][col-1].setCard(card.GetComponent<CardView>());
-    }
-
     // STATE MACHINE FUNCTIONS START HERE
-    
+
     // Handles the player aspect of the what happens after pressing Fight
     public void updatePlayerActions()
     {
